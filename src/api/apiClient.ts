1a41873592b0edb7bd13651e0fbd7698a0bd4a33
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-11-01 19:57:56 +0530

    fix: update API base URL and enhance category management features in CategoriesScreen

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'; 
 
-// const API_BASE_URL = 'http://localhost:3001/api/v1'; 
-const API_BASE_URL = 'http://10.199.96.4:3001/api/v1';
+const API_BASE_URL = 'http://10.101.155.4:3001/api/v1';
 
 
 export const api = axios.create({
