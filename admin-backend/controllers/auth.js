5a5d08e90d9574469adc5130a6a6d5c4e0e4fe89
saranshtyagi <18saranshtyagi2004@gmail.com>
2025-11-16 20:33:16 +0530

    feat: add google login functionality and refactor signUp method

--- admin-backend/controllers/auth.js
+++ admin-backend/controllers/auth.js
@@ -3,26 +3,28 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const { OAuth2Client } = require("google-auth-library");
 const logger = require("../utils/logger");
 const { sendEmailWithRetry } = require("../utils/emailQueue");
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 async function signUp(req, res) {
-    const {email, password, phone} = req.body;
-    try {
-        const existingUser = await prisma.user.findUnique({ where: { email } });
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await prisma.user.create({
-            data: { email, password: hashedPassword, phone },
-        });
-        const token = jwt.sign({userId:user?.id}, process.env.JWT_SECRET);
-        const mailOptions = {
-            from: `${process.env.EMAIL_FROM_NAME} <${process.env.EMAIL_USER}>`,
-            to: user?.email,
-            subject: 'Welcome to Our Platform',
-            html: `
+  const { email, password, phone } = req.body;
+  try {
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: { email, password: hashedPassword, phone },
+    });
+    const token = jwt.sign({ userId: user?.id }, process.env.JWT_SECRET);
+    const mailOptions = {
+      from: `${process.env.EMAIL_FROM_NAME} <${process.env.EMAIL_USER}>`,
+      to: user?.email,
+      subject: "Welcome to Our Platform",
+      html: `
             <div style='font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px;'>
             <img src="https://media.istockphoto.com/id/1321617070/vector/health-medical-logo.jpg?s=612x612&w=0&k=20&c=sus8vhG3c__vCdvOBLDhuf2vPUgIAudIAeUBApU_7Ew=" alt="App Logo" style="width: 100px; display: block; margin: 0 auto 20px" />
             <h1 style="color: #16a34a; text-align: center;">Welcome to Our Platform!</h1>
@@ -30,17 +32,21 @@ async function signUp(req, res) {
             <p style="font-size: 12px; color: #666; text-align: center;">Reply to this message or open it in the App</p>
             <hr style="border: none; border-top: 1px solid #eee; margin: 20px 0;" />
             <p style="font-size: 12px; color: #666; text-align: center;">If you no longer wish to recieve this emails, <a href="https://yourapp.com/unsubscribe?email=${encodeURIComponent(
-                user?.email
+              user?.email
             )}" style="color: #2563eb;">Unsubscribe here</a>.</p>
             </div>
-            `
-        }; 
-        sendEmailWithRetry(mailOptions).catch(error => logger.error(`Failed to send welcome email to ${user?.email}: ${error.message}`));
-        res.json({ user, token });
-    } catch (error) {
-        logger.error(error);
-        throw error;
-    }
+            `,
+    };
+    sendEmailWithRetry(mailOptions).catch((error) =>
+      logger.error(
+        `Failed to send welcome email to ${user?.email}: ${error.message}`
+      )
+    );
+    res.json({ user, token });
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
 }
 
 // async function signUp(req, res) {
@@ -103,22 +109,58 @@ async function signUp(req, res) {
 // }
 
 async function login(req, res) {
-    const { email, password } = req.body;
-    try {
-        const user = await prisma.user.findUnique({ where: { email } });
-        if (!user || !user.password) {
-            return res.status(400).json({ error: "Invalid credentials" });
-        }
-        const match = await bcrypt.compare(password, user.password);
-        if (!match) {
-            return res.status(400).json({ error: "Invalid credentials" });
-        }
-        const token = jwt.sign({ userId: user?.id }, process.env.JWT_SECRET);
-        res.json({ user, token });
-    } catch (error) {
-        logger.error(error);
-        throw error;
+  const { email, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user || !user.password) {
+      return res.status(400).json({ error: "Invalid credentials" });
     }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+    const token = jwt.sign({ userId: user?.id }, process.env.JWT_SECRET);
+    res.json({ user, token });
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
 }
 
-module.exports = { signUp, login };
+async function googleLogin(req, res) {
+  const { idToken } = req.body;
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    const { sub: googleId, email, name } = payload;
+
+    let user = await prisma.user.findFirst({
+      where: { googleId },
+    });
+    if (!user) {
+      user = await prisma.user.findUnique({ where: { email } });
+
+      if (user) {
+        user = await prisma.user.update({
+          where: { email },
+          data: { googleId },
+        });
+      } else {
+        user = await prisma.user.create({
+          data: { email, googleId, name },
+        });
+      }
+    }
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET,);
+    res.json({ user, token });
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+}
+
+module.exports = { signUp, login, googleLogin };
